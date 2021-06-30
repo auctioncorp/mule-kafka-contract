a821d3b1fb9336c947eb4c7eee22e6aa5be7e66a
jgalindo-auction <jgalindo@auction.com>
2021-06-30 10:27:21 -0700

    fixed kafka avro transformer

---
+++ src/main/java/com/auction/sfdc/contract/transform/Transformer.java
@@ -0,0 +1,67 @@
+package com.auction.sfdc.contract.transform;
+
+/*
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.avro.file.DataFileStream;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.apache.kafka.common.serialization.StringSerializer;
+*/
+
+import org.mule.transformer.AbstractMessageTransformer;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
+
+public abstract class Transformer  extends AbstractMessageTransformer{
+	
+	public Transformer() {}
+	
+	public static String parseIncoming(final byte[] b, final String schemaRegistryURL) {
+		System.out.println ( "schemaRegistryURL: " + schemaRegistryURL );
+        final SchemaRegistryClient client = (SchemaRegistryClient)new CachedSchemaRegistryClient(schemaRegistryURL, 3);
+        final KafkaAvroDeserializer des = new KafkaAvroDeserializer(client);
+        Object o = null;
+        try {
+            o = des.deserialize("transactions", b);
+            System.out.println(o.getClass());
+            System.out.println(o.toString());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            des.close();
+        }
+        return o.toString();
+    }
+	
+	public static String createOutbound(final String jsonBody, final String schemaRegistryURL) {
+		System.out.println ( "schemaRegistryURL: " + schemaRegistryURL );
+        final SchemaRegistryClient client = (SchemaRegistryClient)new CachedSchemaRegistryClient(schemaRegistryURL, 3);
+        final KafkaAvroSerializer des = new KafkaAvroSerializer(client);
+        Object o = null;
+        try {
+            o = des.serialize("transactions", jsonBody);
+            System.out.println(o.getClass());
+            System.out.println(o.toString());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            des.close();
+        }
+        return o.toString();
+    }
+
+}
